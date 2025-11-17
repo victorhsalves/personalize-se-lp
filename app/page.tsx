bf9ad4dbe0d4146d9c14bcc63973a58a4d5b0497
Victor <victorhugo.vhs@hotmail.com>
2025-11-17 17:21:50 -0300

    adicionado carrocel de vídeo na home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import ProductGrid from "@/components/ProductGrid";
+import ProductVideos from "@/components/ProductVideos";
 import Testimonials from "@/components/Testimonials";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
@@ -42,6 +43,7 @@ export default function Home() {
         <Hero />
         <About />
         <ProductGrid />
+        <ProductVideos />
         <Testimonials />
         <CTA />
       </main>
